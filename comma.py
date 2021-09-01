ce0c6686dd9d722e3d604d68ddbdb9c257d662eb
jerm117 <89803157+jerm117@users.noreply.github.com>
2021-09-01 10:27:33 -0400

    Add files via upload

---
+++ comma.py
@@ -0,0 +1,19 @@
+# practice project
+# comma code
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+def listStr(list):
+
+
+##    for i in list:
+##        print(list[i:-1], sep=", ")
+
+
+    
+##    list.insert(-1, 'and ')
+##    newlist = ', '.join(list)
+##    print(newlist)
+
+listStr(spam)
+
